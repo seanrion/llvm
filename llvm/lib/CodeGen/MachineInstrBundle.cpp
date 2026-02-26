420781cf73bedb2969e02f5a0f8ff5c2205e4007
Sean <45787907+seanrion@users.noreply.github.com>
2026-02-26 11:15:28 +0800

    [CC:ShrinkWrap] Support MI bundles in eliminateFrameIndex.

--- llvm/lib/CodeGen/MachineInstrBundle.cpp
+++ llvm/lib/CodeGen/MachineInstrBundle.cpp
@@ -144,12 +144,20 @@ void llvm::finalizeBundle(MachineBasicBlock &MBB,
   SmallSet<Register, 8> UndefUseSet;
   SmallVector<std::pair<Register, Register>> TiedOperands;
   SmallVector<MachineInstr *> MemMIs;
+  SmallSet<int, 8> FIUses;
   for (auto MII = FirstMI; MII != LastMI; ++MII) {
     // Debug instructions have no effects to track.
     if (MII->isDebugInstr())
       continue;
 
-    for (MachineOperand &MO : MII->all_uses()) {
+    for (MachineOperand &MO : MII->operands()) {
+      if (MO.isFI()) {
+        FIUses.insert(MO.getIndex());
+        continue;
+      }
+
+      if (!MO.isReg()  || !MO.isUse())
+        continue;
       Register Reg = MO.getReg();
       if (!Reg)
         continue;
@@ -237,6 +245,8 @@ void llvm::finalizeBundle(MachineBasicBlock &MBB,
   }
 
   MIB->cloneMergedMemRefs(MF, MemMIs);
+  for (int FI : FIUses)
+    MIB.addFrameIndex(FI);
 }
 
 /// finalizeBundle - Same functionality as the previous finalizeBundle except
