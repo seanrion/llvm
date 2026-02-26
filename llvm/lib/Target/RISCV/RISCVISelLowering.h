ed8736338d7822583c4751ca6ddcf9bfc3a95ab1
Sean <45787907+seanrion@users.noreply.github.com>
2026-02-26 11:14:56 +0800

    [CC:ShrinkWrap] Make the RA to save CSRs.

--- llvm/lib/Target/RISCV/RISCVISelLowering.h
+++ llvm/lib/Target/RISCV/RISCVISelLowering.h
@@ -643,6 +643,8 @@ private:
 
   std::pair<const TargetRegisterClass *, uint8_t>
   findRepresentativeClass(const TargetRegisterInfo *TRI, MVT VT) const override;
+  
+  void finalizeLowering(MachineFunction &MF) const override;
 };
 
 namespace RISCVVIntrinsicsTable {
